14ebe3abf10ea0346f812ecad01c6bf285daf963
Luuk Verweij <luuk_verweij@msn.com>
2024-04-12 13:50:18 +0200

    use BitSet instead of set[int] for speedup

--- src/flood/players/graph/graph.py
+++ src/flood/players/graph/graph.py
@@ -1,4 +1,5 @@
 from flood.board import Board
+from flood.players.graph.types import BitSet
 
 
 def get_node_ids(board: Board) -> list[int]:
@@ -104,7 +105,15 @@ def print_node_neighbours(node_neighbours: list[set[int]]) -> None:
 class Graph:
     def __init__(self, colors: list[int], neighbours: list[set[int]]) -> None:
         self.colors = colors
-        self.neighbours = neighbours
+
+        # node id -> bitset of neighbour node ids
+        self.neighbours: list[BitSet] = [
+            BitSet.from_set(node_neighbours) for node_neighbours in neighbours
+        ]
+
+        self.color_sets = [BitSet(0) for _ in range(max(self.colors) + 1)]
+        for index, color in enumerate(self.colors):
+            self.color_sets[color] = BitSet(self.color_sets[color] | 1 << index)
 
     def node_count(self) -> int:
         return len(self.neighbours)
--- src/flood/players/graph/solver.py
+++ src/flood/players/graph/solver.py
@@ -2,11 +2,7 @@ from copy import copy
 import datetime
 from flood.board import Board
 from flood.players.graph.graph import Graph
-
-
-class SolutionFound(Exception):
-    def __init__(self, moves: list[int]) -> None:
-        self.moves = moves
+from flood.players.graph.types import BitSet, SolutionFound
 
 
 class GraphSinglePlayerSolver:
@@ -19,23 +15,23 @@ class GraphSinglePlayerSolver:
         self.solve_start = datetime.datetime.now()
         self.attempts = 0
 
-    def _get_newly_flooded(self, flooded: set[int], move: int) -> set[int]:
-        newly_flooded: set[int] = set()
+    def _get_newly_flooded(self, flooded: BitSet, move: int) -> BitSet:
+        unflooded_color_set = BitSet(self.graph.color_sets[move] & ~flooded)
+        after = BitSet(0)
+        for node in unflooded_color_set:
+            neighbours = self.graph.neighbours[node]
+            if neighbours & flooded:
+                after = BitSet(after | (1 << node))
 
-        for flooded_node in flooded:
-            for neighbour_node in self.graph.neighbours[flooded_node]:
-                if (
-                    neighbour_node not in flooded
-                    and self.graph.colors[neighbour_node] == move
-                ):
-                    newly_flooded.add(neighbour_node)
+        return after
 
-        return newly_flooded
+    def _count_unflooded_colors(self, flooded_bitset: BitSet) -> int:
+        unflooded_colors = 0
+        for color_set in self.graph.color_sets:
+            if color_set & ~flooded_bitset:
+                unflooded_colors += 1
 
-    def _count_unflooded_colors(self, flooded: set[int]) -> int:
-        unflooded_nodes = set(range(self.graph.node_count())) - flooded
-        unflooded_colors = {self.graph.colors[node] for node in unflooded_nodes}
-        return len(unflooded_colors)
+        return unflooded_colors
 
     def _print_speed(self) -> None:
         seconds = (datetime.datetime.now() - self.solve_start).total_seconds()
@@ -44,14 +40,14 @@ class GraphSinglePlayerSolver:
             f"{self.attempts:>10} attempts / {seconds:7.2f} sec = {speed:6.0f} attempts / sec"
         )
 
-    def _solve(self, flooded: set[int], moves: list[int]) -> None:
+    def _solve(self, flooded: BitSet, moves: list[int]) -> None:
         if len(moves) > self.max_moves:
             return
 
         if self._count_unflooded_colors(flooded) + len(moves) > self.max_moves:
             return
 
-        if len(flooded) == self.graph.node_count():
+        if flooded.count() == self.graph.node_count():
             raise SolutionFound(moves)
 
         self.attempts += 1
@@ -62,11 +58,11 @@ class GraphSinglePlayerSolver:
         if moves:
             valid_moves -= {moves[-1]}
 
-        move_tuples: list[tuple[int, int, set[int]]] = []
+        move_tuples: list[tuple[int, int, BitSet]] = []
 
         for move in valid_moves:
             newly_flooded = self._get_newly_flooded(flooded, move)
-            heuristic = len(newly_flooded)
+            heuristic = newly_flooded.count()
             move_tuple = (move, heuristic, newly_flooded)
 
             if newly_flooded:
@@ -75,7 +71,8 @@ class GraphSinglePlayerSolver:
         move_tuples = sorted(move_tuples, key=lambda t: -t[1])
 
         for move, _, newly_flooded in move_tuples:
-            self._solve(flooded | newly_flooded, copy(moves) + [move])
+            child_flooded = BitSet(flooded | newly_flooded)
+            self._solve(child_flooded, copy(moves) + [move])
 
     def solve(self) -> int:
         self.solve_start = datetime.datetime.now()
@@ -87,9 +84,12 @@ class GraphSinglePlayerSolver:
         best_move = -1
 
         while True:
+            initial_flooded = BitSet(1 << self.start_node_id)
+
             try:
-                self._solve({self.start_node_id}, [])
+                self._solve(initial_flooded, [])
             except SolutionFound as solution:
+                self._print_speed()
                 print(f"Found solution ({len(solution.moves)}): ", end="")
                 print(
                     " ".join(
@@ -103,8 +103,6 @@ class GraphSinglePlayerSolver:
                 best_move = solution.moves[0]
             else:
                 break
-
-        self._print_speed()
 
         assert best_move != -1
         return best_move
